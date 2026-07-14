c5c2afb6e4e76e211a48a92abc5a9550389e9f62
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-07-14 11:04:45 +0300

    set port number

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
+
+const PORT = process.env.PORT;
 
 app.get('/', (req,res) =>{
     res.send('Server is running on node')
     
 });
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     
-    console.log("Server running on PORT: 3000 ")
+    console.log(`Server running on PORT: ${PORT} `)
 });
