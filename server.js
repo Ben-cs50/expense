6a9846ca1f60634728970814bf93031ca9c3b92b
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-07-14 12:36:19 +0300

    add stop server process when server is killed

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-require('dotenv').config();
-const express = require('express');
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config(); 
 
 const app = express();
+const PORT = process.env.PORT 
 
-const PORT = process.env.PORT;
-
-app.get('/', (req,res) =>{
-    res.send('Server is running on node')
-    
+app.get('/', (req, res) => {
+    res.send('Server is running on node');
 });
 
-app.listen(PORT, () =>{
+const server = app.listen(PORT, () => {
     
-    console.log(`Server running on PORT: ${PORT} `)
+    console.log(`Server running on PORT: ${PORT}`);
+
+});
+
+process.on('SIGINT', () => {
+    console.log('\nShutting down gracefully...');
+    
+    server.close(() => {
+        console.log('Server has stopped listening for connections.');
+        process.exit(0);
+    });
 });
