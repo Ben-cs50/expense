ec137bc9b2c584936c1037dcd9be62bfd8a169c7
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-07-14 17:11:21 +0300

    add getTransactionById endpoint

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,7 +1,12 @@
 export const listTransactions = (req, res) => {
-    console.log("A list of all transactions",);
+    console.log("A list of all transactions");
     console.log(JSON.stringify(req.query));
     console.log(JSON.stringify(req.params));
 
     res.status(200).json({message: "All transactions are listed"})
+}
+export const getTransactionById = (req, res) => {
+    console.log("Fetching transaction with Id");
+    console.log(JSON.stringify(req.params));
+    res.status(200).json({message:`Transaction with ID ${req.params.id}`})
 }
--- routes/transactions.route.js
+++ routes/transactions.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import {listTransactions} from '../controllers/transaction.controller.js'
+import {getTransactionById} from '../controllers/transaction.controller.js'
+
+
 
 const router = express.Router();
 
 router.get('/transactions', listTransactions);
+router.get('/transactions/:id', getTransactionById);
 
 export default router;
