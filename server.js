87d3a2ee2fd7579b5556557a770feeab281396ef
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-07-20 17:22:05 +0300

    configuring database connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from "dotenv"
+import express from "express"
+import mysql from "mysql2/promise"
 import smsWebhook from './routes/smsWebhook.route.js'
 import transactions from './routes/transactions.route.js'
 import getTransactionById from './routes/transactions.route.js'
@@ -12,15 +13,43 @@ import createCategoryRule from './routes/categories.route.js'
 import listCategoryRule from './routes/categories.route.js'
 
 
+
 dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT 
 
+// db connection
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10
+});
+
+// error defination
+pool.getConnection((err) => {
+    if(err){
+        console.error("Database connection failed: " + err.stack);
+        return;
+    }
+    console.log("Database connected successfully")
+});
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is running on node');
+app.get('/', async (req, res) => {
+    try{
+    const [rows] = await pool.query('SELECT * FROM raw_messages');
+          
+            res.json(rows);
+
+        }catch(err){
+            return res.status(500).json({error: err.message});
+        }
+    // );
+    // res.send('Server is running on node');
 });
 
 app.use('/api', smsWebhook);
@@ -51,3 +80,5 @@ process.on('SIGINT', () => {
         process.exit(0);
     });
 });
+
+
