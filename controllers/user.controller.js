2c31adcd534adfa109b991d9046ab86d45c957e2
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-07-23 16:14:03 +0300

    add email first and last name to request body

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,7 +10,7 @@ export const user = async (req, res) => {
 
     // console.log("User validated")
 
-    const {username, password} = req.body
+    const {username, password, email, first_name, last_name} = req.body
 
     // const newUsers = {...req.body}
 
@@ -24,13 +24,13 @@ export const user = async (req, res) => {
     // }
 
         try{
-    const query = 'INSERT INTO users(username, password) VALUES (?,?) ';
-    const [result] = await db.query(query, [username, password]);
+    const query = 'INSERT INTO users(username, password, email, first_name, last_name) VALUES (?,?,?,?,?) ';
+    const [result] = await db.query(query, [username, password, email, first_name, last_name]);
           
-            res.status(201).json({message: "User created successfully"})
+            res.status(200).json({message: "User created successfully"})
 
         }catch(err){
-            return res.status(500).json({error: "Database insertion failed " + err.message});
+            return res.status(400).json({error: "Database insertion failed " + err.message});
         }
     // );
     // res.send('Server is running on node');
